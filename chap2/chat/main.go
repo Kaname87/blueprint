138b94c1dcef792000235b33d86f505b5cda7d5a
kaname87 <kaname.nakanishi@bibo.com.ph>
2016-11-19 19:44:15 +0800

    finish chap2

---
+++ chap2/chat/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	// "github.com/Kaname87/blueprint/chap2/trace"
+	"log"
+	"net/http"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	// "os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/objx"
+)
+
+// tmpl represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+func main() {
+	// root
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() //parse the flag
+	// setup gomniauth
+	gomniauth.SetSecurityKey("TSTSTS")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret",
+			"http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret",
+			"http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret",
+			"http://localhost:8080/auth/callback/google"),
+	)
+
+	r := newRoom()
+	//r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+	// start web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ chap2/chat/room.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Kaname87/blueprint/chap2/trace"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
+)
+
+type room struct {
+	// forward is channel that holds incoming messages
+	// that should be forwardesd to the other clients
+	forward chan *message
+	// join is a channel for clients wishing to join a room.
+	join chan *client
+	// leave is channel for clients wishing to leave the room.
+	leave chan *client
+	// clients holds all current clients in this room
+	clients map[*client]bool
+	// tracer will receive trace information of activity in the room
+	tracer trace.Tracer
+}
+
+// newRoom makes a new room.
+func newRoom() *room {
+	return &room{
+		forward: make(chan *message),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+			r.tracer.Trace("New client joined")
+
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("Clent left")
+		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", msg.Message)
+			// forwarding message to all clients
+			for client := range r.clients {
+				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
+	WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie:", err)
+		return
+	}
+	client := &client{
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
